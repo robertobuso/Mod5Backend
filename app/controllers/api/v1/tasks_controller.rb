876d70fde69083e4e06bd480273229f5ae9dc4e9
robertobuso <robertobuso@gmail.com>
2018-10-13 19:22:42 -0400

    connected backend and frontend through heroku

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::TasksController < ApplicationController
+  before_action :find_task, only: [:update]
+
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def update
+    @task.update(task_params)
+    if @task.save
+      render json: @task, status: :accepted
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task, status: :accepted
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.permit(:category, :completed, :experience_id)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
+end
